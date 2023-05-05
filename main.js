6aa788a788ae86971fc7d963808e360a6a3a9099
Aryadeep <arya.d.chowdhury+github@gmail.com>
2023-05-05 13:56:13 +0530

    minor changes

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 let myImage = document.getElementById("myimage");
-myImage.src = 'aryadeep.jpg'
+myImage.src = 'aryadeep.jpg';
+
+function changeText(params) {
+    let el = document.getElementById("para");
+    el.style.display = flex;
+    el.innerHTML = "";
+}
+
+function changeTheme(params) {
+    let theme = document.getElementById("body")
+    theme.style.backgroundColor = white;
+
+    element => function()
+    {
+        this.element.style.changeText = "";
+    }
+}
